d49174165e45103da06252b707bafe455ed09db9
Ritik <ritik221jaggi@gmail.com>
2023-11-22 20:30:33 +0530

    like button in posts

--- src/Components/Posts/Posts.jsx
+++ src/Components/Posts/Posts.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./Posts.scss"
 import { posts } from '../../assets/profileData'
+import { FiThumbsUp } from 'react-icons/Fi'
 const Posts = () => {
     return (
         <div className='container3'>
@@ -11,7 +12,7 @@ const Posts = () => {
             {posts.map((item, index) => {
                 return (
                     <div className="postview">
-                        <div className="postheading">{item.heading}</div>
+                        <div className="postheading">{item.heading}<FiThumbsUp/></div>
                         <div className="postcontent">{item.content}</div>
                         <div className='by'>
                             <div className="name">{item.by}</div>
